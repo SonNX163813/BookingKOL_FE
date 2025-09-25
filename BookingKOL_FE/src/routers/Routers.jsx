6f6d287779f0d84e2ebe2249a476fa28149ff56a
vovananh1124 <vovananh1124@gmail.com>
2025-09-26 01:35:14 +0700

    add login and register

--- BookingKOL_FE/src/routers/Routers.jsx
+++ BookingKOL_FE/src/routers/Routers.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
+import LoginPage from "../pages/authentication/LoginPage";
+import RegisterPage from "../pages/authentication/RegisterPage";
+
 // import Infomation from "../pages/profile/owner/infomation";
 // import About from "../pages/About";
 
@@ -8,6 +11,8 @@ const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       {/* <Route path="/infomation" element={<Infomation />} />
       <Route path="/about" element={<About />} /> */}
     </Routes>
