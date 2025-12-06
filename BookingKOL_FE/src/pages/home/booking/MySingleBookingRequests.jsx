0a4fb6c64cbd59aab69e5a0083db943560187ffc
SonNX163813 <108038889+SonNX163813@users.noreply.github.com>
2025-12-06 21:49:05 +0700

    fix: update BASE_URL to point to the correct API endpoint

--- BookingKOL_FE/src/pages/home/booking/MySingleBookingRequests.jsx
+++ BookingKOL_FE/src/pages/home/booking/MySingleBookingRequests.jsx
@@ -547,7 +547,9 @@ const MySingleBookingRequests = () => {
       const normalizedContractStatus = contractStatusSource
         ? contractStatusSource.toString().toUpperCase()
         : null;
-      const isRefundable = normalizedPaymentStatus === "PAID";
+      const isRefundable =
+        normalizedPaymentStatus === "PAID" &&
+        normalizedContractStatus !== "COMPLETED";
       // &&        normalizedContractStatus !== "WAIT_FOR_REFUND";
       const isPaymentPending = normalizedPaymentStatus === "PENDING";
       const isProcessingPaymentCancelThisRow =
